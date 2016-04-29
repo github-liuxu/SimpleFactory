9594803b80fc988f20cbbbd9d3e56216119a7b05
github-liuxu <liudongxu@macbook-pro.local>
2016-04-29 17:25:35 +0800

    Initial Commit

---
+++ SimpleFactory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleFactory
+//
+//  Created by 刘东旭 on 16/4/29.
+//  Copyright © 2016年 刘东旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleFactory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleFactory
+//
+//  Created by 刘东旭 on 16/4/29.
+//  Copyright © 2016年 刘东旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
